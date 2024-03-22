e4b44284c5e21254b661cf1060669c52bc9bb90f
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-22 10:12:47 +0800

    fixed orderId cannot finded problem

--- app/Loader.php
+++ app/Loader.php
@@ -22,6 +22,9 @@ class Loader
             10,
             [
                 'orderId' => function ($tx) {
+                    if (!isset($tx->orderId)) {
+                        return esc_html__('Waiting...', 'wpforms-cryptopay');
+                    }
                     return Helpers::run('view', 'components/link', [
                         'url' => sprintf(admin_url('admin.php?page=wpforms-payments&view=payment&payment_id=%d'), $tx->orderId), // @phpcs:ignore
                         'text' => sprintf(esc_html__('View payment #%d', 'wpforms-cryptopay'), $tx->orderId)
