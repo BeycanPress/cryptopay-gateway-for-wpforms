0ada0dfc48fbf3c0308bd17c1e3c03e41e11c553
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-04-04 16:48:16 +0800

    updated for wp.org

--- cryptopay-gateway-for-wpforms.php
+++ cryptopay-gateway-for-wpforms.php
@@ -10,7 +10,7 @@ defined('ABSPATH') || exit;
 // @phpcs:disable Generic.Files.LineLength
 
 /**
- * Plugin Name: WPForms - CryptoPay Gateway
+ * Plugin Name: CryptoPay Gateway for WPForms
  * Version:     1.0.0
  * Plugin URI:  https://beycanpress.com/cryptopay/
  * Description: Adds Cryptocurrency payment gateway (CryptoPay) for WPForms.
@@ -21,7 +21,7 @@ defined('ABSPATH') || exit;
  * Text Domain: wpforms-cryptopay
  * Tags: Cryptopay, Cryptocurrency, WooCommerce, WordPress, MetaMask, Trust, Binance, Wallet, Ethereum, Bitcoin, Binance smart chain, Payment, Plugin, Gateway, Moralis, Converter, API, coin market cap, CMC
  * Requires at least: 5.0
- * Tested up to: 6.4.3
+ * Tested up to: 6.5.0
  * Requires PHP: 8.1
 */
 
