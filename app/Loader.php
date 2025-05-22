71e9fce320627ae7f6c856ac57d814ffff2c26fb
BeycanDeveloper <halilbeycan0@gmail.com>
2025-05-22 16:59:46 +0800

    .

--- app/Loader.php
+++ app/Loader.php
@@ -15,23 +15,25 @@ class Loader
     {
         Helpers::registerIntegration('wpforms');
 
-        // add transaction page
-        Helpers::createTransactionPage(
-            esc_html__('WPForms Transactions', 'wpforms-cryptopay'),
-            'wpforms',
-            10,
-            [
-                'orderId' => function ($tx) {
-                    if (!isset($tx->orderId)) {
-                        return esc_html__('Waiting...', 'wpforms-cryptopay');
+        add_action('init', function (): void {
+            // add transaction page
+            Helpers::createTransactionPage(
+                esc_html__('WPForms Transactions', 'wpforms-cryptopay'),
+                'wpforms',
+                9,
+                [
+                    'orderId' => function ($tx) {
+                        if (!isset($tx->orderId)) {
+                            return esc_html__('Waiting...', 'wpforms-cryptopay');
+                        }
+                        return Helpers::run('view', 'components/link', [
+                            'url' => sprintf(admin_url('admin.php?page=wpforms-payments&view=payment&payment_id=%d'), $tx->orderId), // @phpcs:ignore
+                            /* translators: %d: transaction order id */
+                            'text' => sprintf(esc_html__('View payment #%d', 'wpforms-cryptopay'), $tx->orderId)
+                        ]);
                     }
-                    return Helpers::run('view', 'components/link', [
-                        'url' => sprintf(admin_url('admin.php?page=wpforms-payments&view=payment&payment_id=%d'), $tx->orderId), // @phpcs:ignore
-                        /* translators: %d: transaction order id */
-                        'text' => sprintf(esc_html__('View payment #%d', 'wpforms-cryptopay'), $tx->orderId)
-                    ]);
-                }
-            ]
-        );
+                ]
+            );
+        });
     }
 }
